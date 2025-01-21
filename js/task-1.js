addd1cfc4b9a415d71629882cbaf7975e6fc3b87
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-01-21 12:41:07 +0200

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,10 @@
-function isEnoughCapacity(products, containerSize) {
-  
-    const totalItems = Object.values(products).reduce((sum, quantity) => sum + quantity, 0);
-        return totalItems <= containerSize;
-}
+const slugify = (title) => {
+  return title
+    .toLowerCase()       
+    .split(' ')          
+    .join('-');           
 
-
-
-
-console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-);
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-);
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-);
-console.log(
-  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-);
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
